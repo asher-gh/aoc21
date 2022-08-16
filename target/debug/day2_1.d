f9afc4755deaa87a9c08948e2ab0cfafdb99db37
Ashish <ashish.2017@hotmail.com>
2022-08-16 22:01:45 +0100

    day 2.1

---
+++ target/debug/day2_1.d
@@ -0,0 +1,1 @@
+/Users/asher/Repos/Studies/coding_practice/AOC/2021/target/debug/day2_1: /Users/asher/Repos/Studies/coding_practice/AOC/2021/day2_1/src/main.rs
---
+++ target/debug/deps/day2_2-6d8af1cf9ff83026.d
@@ -0,0 +1,5 @@
+/Users/asher/Repos/Studies/coding_practice/AOC/2021/target/debug/deps/day2_2-6d8af1cf9ff83026.rmeta: day2_2/src/main.rs
+
+/Users/asher/Repos/Studies/coding_practice/AOC/2021/target/debug/deps/day2_2-6d8af1cf9ff83026.d: day2_2/src/main.rs
+
+day2_2/src/main.rs:
